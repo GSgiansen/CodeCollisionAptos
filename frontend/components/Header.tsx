6a8dd12c785ccb2b6e593a3c203059fafb408f0e
jinyuan0425 <pdmon123@gmail.com>
2024-08-08 15:44:54 +0800

    Remove sidebar
    Added links back to header and removed sidebar

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -2,31 +2,36 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { WalletSelector } from "./WalletSelector";
 import { IS_DEV } from "@/constants";
-import Sidebar from "./Sidebar";
 
 export function Header() {
-  const [isSidebarOpen, setSidebarOpen] = useState(false);
-
-  const toggleSidebar = () => {
-    setSidebarOpen(!isSidebarOpen);
-  };
-
   return (
     <>
       <div className="bg-techno-header">
         <div className="header-container flex items-center px-4 py-2 max-w-screen-xl mx-auto w-full">
-          <button
-            className="header-sidebar-btn"
-            onClick={toggleSidebar}
-          >
-            ☰
-          </button>
           <h1 className="display flex-grow mx-4">
             <Link to="/">Aptos Ticket Master</Link>
           </h1>
+          <div className="flex gap-2 items-center ml-auto">
+            {IS_DEV && (
+              <>
+                <Link className="header-link" to="/my-profile">
+                  My Profile
+                </Link>
+                <Link className="header-link" to="/my-collections">
+                  View Concerts
+                </Link>
+                <Link className="header-link" to="/marketplace">
+                  Marketplace
+                </Link>
+                <Link className="header-link" to="/create-collection">
+                  Contact Us
+                </Link>
+              </>
+            )}
+            <WalletSelector />
+          </div>
         </div>
       </div>
-      {isSidebarOpen && <Sidebar onClose={toggleSidebar} />}
     </>
   );
 }
